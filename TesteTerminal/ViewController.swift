6ae420daeb9bee70b1ead3a0ec5a6fa2658661b7
Karen Makihara <karen_yoshimura@yahoo.com.br>
2020-10-08 19:54:16 -0300

    Secund commit

---
+++ TesteTerminal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesteTerminal
+//
+//  Created by Fabio Makihara on 08/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
